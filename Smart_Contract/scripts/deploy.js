baa1001282fdcb8f1fd1896ac45d68b456d27354
AdityaPatadiya <patadiyaaditya0079@gmail.com>
2025-08-06 11:23:39 +0000

    smart contract is deployes using hardhat

--- Smart_Contract/scripts/deploy.js
+++ Smart_Contract/scripts/deploy.js
@@ -1,12 +1,14 @@
+const { ethers } = require("hardhat");
+
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying with:", deployer.address);
 
   const PaymentContract = await ethers.getContractFactory("PaymentContract");
   const contract = await PaymentContract.deploy();
-  await contract.deployed();
+  await contract.waitForDeployment();
 
-  console.log("Contract deployed at:", contract.address);
+  console.log("Contract deployed at:", await contract.getAddress());
 }
 
 main().catch((error) => {
