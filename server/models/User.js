3820b32a5f38d14517c5c610e612c43ed7d79bb5
AdityaPatadiya <patadiyaaditya0079@gmail.com>
2025-08-11 02:26:11 +0000

    reformat the frontend part

--- server/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  uid: String, // Firebase UID
-});
-
-module.exports = mongoose.model('User', userSchema);
